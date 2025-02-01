0478635bf0346cbfa546586889b4c89daf4269f9
codesensei-tushar <tusharumbarkar2022@gmail.com>
2025-02-01 12:24:28 +0530

    ook

--- project/src/main.py
+++ project/src/main.py
@@ -2,7 +2,7 @@ import torch
 from utils.config import Config
 from data.dataset import get_dataloaders
 from models.conv import CNN
-from src.train import train_model
+from train import train_model
 
 def main():
     config = Config()
